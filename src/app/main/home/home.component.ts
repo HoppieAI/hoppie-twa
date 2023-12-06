9b60e563ca6a24461727602fa51c0557141488bd
evgenyaveryanov <evgavery@gmail.com>
2023-12-06 16:16:49 +0300

    TG window object

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TelegramService} from "../../services/telegram.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  username: string;
+  constructor(
+    private tgService: TelegramService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.username = this.tgService.username ? this.tgService.username : 'User';
+  }
 
 }
